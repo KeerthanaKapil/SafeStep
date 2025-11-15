1dffb678536f9019409039b7c18e4ccfa2dc6d26
KeerthanaKapil <Keerthana.selvaraj585@gmail.com>
2025-11-14 20:35:46 -0600

    Update TipListActivity.kt

--- app/src/main/java/com/example/safestep/TipListActivity.kt
+++ app/src/main/java/com/example/safestep/TipListActivity.kt
@@ -2,33 +2,68 @@ package com.example.safestep.tips
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.safestep.R
+import com.example.safestep.ContactsActivity
+import com.example.safestep.MainActivity
+import com.example.safestep.ProfileActivity
+import com.example.safestep.databinding.ActivityTipListBinding
 
+/**
+ * Tips List: shows safety tip categories + bottom navigation.
+ */
 class TipListActivity : AppCompatActivity() {
+
+    private lateinit var b: ActivityTipListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tip_list)
+        b = ActivityTipListBinding.inflate(layoutInflater)
+        setContentView(b.root)
 
-        // Connect each button from XML and set click
-        val btnWork = findViewById<Button>(R.id.btnWork)
-        val btnHome = findViewById<Button>(R.id.btnHome)
-        val btnUni = findViewById<Button>(R.id.btnUni)
-        val btnOnline = findViewById<Button>(R.id.btnOnline)
-        val btnStreet = findViewById<Button>(R.id.btnStreet)
-
-        btnWork.setOnClickListener { openTip(0) }
-        btnHome.setOnClickListener { openTip(1) }
-        btnUni.setOnClickListener { openTip(2) }
-        btnOnline.setOnClickListener { openTip(3) }
-        btnStreet.setOnClickListener { openTip(4) }
+        setupBottomNav()
+        setupTipButtons()
     }
 
-    private fun openTip(index: Int) {
-        val i = Intent(this, TipDetailActivity::class.java)
-        i.putExtra("index", index)
-        startActivity(i)
+    private fun setupBottomNav() {
+
+        // These come from include_bottom_nav.xml
+        val navHome = b.bottomNav.navHome
+        val navProfile = b.bottomNav.navProfile
+        val navContacts = b.bottomNav.navContacts
+
+        navHome.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        navProfile.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+
+        navContacts.setOnClickListener {
+            startActivity(Intent(this, ContactsActivity::class.java))
+        }
+    }
+
+    private fun setupTipButtons() {
+        // Later you can open detail screens here
+        b.btnWork.setOnClickListener {
+            // TODO: open Work safety tips detail
+        }
+
+        b.btnHome.setOnClickListener {
+            // TODO: open Home safety tips detail
+        }
+
+        b.btnUni.setOnClickListener {
+            // TODO: open University safety tips detail
+        }
+
+        b.btnOnline.setOnClickListener {
+            // TODO: open Online safety tips detail
+        }
+
+        b.btnStreet.setOnClickListener {
+            // TODO: open Street safety tips detail
+        }
     }
 }
